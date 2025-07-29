07ebb37c7be7286ef2f0f62cdc356994bdc7eca6
sergeyfedorov02 <sergey_fedorov200001@mail.ru>
2025-07-29 15:06:38 +0300

    Добавьте файлы проекта.

---
+++ IdentTestTask3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace IdentTestTask3
+{
+    internal class Program
+    {
+        private static IEnumerable<Type> GetStandardControls(Assembly assembly)
+        {
+            return assembly.GetTypes()
+                .Where(type => typeof(Control).IsAssignableFrom(type) &&  // получаем все классы из сборки, которые наследуются от Control
+                               type != typeof(Control) &&   // отфильтровываем (исключаем сам Control)
+                               !type.IsAbstract &&          // только те, которые можем создать
+                               !type.IsNested &&            // не вложенные (они вспомогательные)
+                               type.Namespace == "System.Windows.Forms");  // только те, что лежат в указанном пространстве
+        }
+
+        private static void WriteStandardControls(IEnumerable<Type> types)
+        {
+            foreach (var type in types)
+            {
+                Console.WriteLine(type.FullName);
+            }
+        }
+        static void Main(string[] args)
+        {
+            // загружаем сборку assembly (где назодятся контроля WinForms)
+            var assembly = Assembly.LoadFrom("System.Windows.Forms.dll");
+
+            // получаем контролы и выводим их
+            WriteStandardControls(GetStandardControls(assembly));
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,22 @@
+# IdentTestTask5
+
+## Как собрать и запустить приложение:
+
+1. Клонировать репозиторий
+
+```git clone https://github.com/sergeyfedorov02/IdentTestTask3.git```
+
+2. Перейти в рабочий каталог
+
+```cd IdentTestTask3```
+
+3. Осуществить сборку проекта
+
+```dotnet build --configuration=Release```
+
+4. Перейти в каталог с исполняемым файлом и запустить его
+
+```
+cd IdentTestTask3\bin\Release\net9.0\
+IdentTestTask3.exe
+```
